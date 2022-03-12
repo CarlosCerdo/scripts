7f3432ca07b544591e65d8127ddb1f562b701804
carlos <zxcbnm.an94@qq.com>
2022-03-12 22:27:58 +0800

    my scripts

--- autostart.sh
+++ autostart.sh
@@ -2,9 +2,10 @@
 
 /bin/bash ~/scripts/dwm_refresh.sh &
 /bin/bash ~/scripts/wp_autochange.sh &
+/bin/bash ~/scripts/onedrive_auto.sh &
 
+# ruanjian
 fcitx5 &
-
 picom &
 
 ~/scripts/autostart_wait.sh
